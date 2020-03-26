ae74fdbe46fc4ae801a16d77a1e93c78994fbb71
Kyle Payne <kspayne93@gmail.com>
2020-03-25 22:19:04 -0600

    updated styling

--- src/components/panels/ClientCountPanel/ClientCountPanel.jsx
+++ src/components/panels/ClientCountPanel/ClientCountPanel.jsx
@@ -25,69 +25,71 @@ export default function ClientCountPanel(props) {
 	return (
 		<div className='dashboard-panel client-count-panel'>
 			<p style={{ color: 'white' }}>Stuck Orders</p>
-			<ResponsiveBar
-				data={props.expeditedCount}
-				keys={[ 'Non-Expedited', 'Expedited' ]}
-				indexBy="client"
-				margin={{ top: 0, right: 150, bottom: 50, left: 150 }}
-				padding={0.3}
-				layout="horizontal"
-				colors={['#8b2abb', '#ef7a00']}
-				theme = {theme}
-				// borderWidth={3}
-				// borderColor={'white'}
-				borderRadius={5}
-				index='color'
-				axisTop={null}
-				axisRight={null}
-				axisBottom={{
-						tickSize: 5,
-						tickPadding: 5,
-						tickRotation: 0,
-						legend: 'Total',
-						legendPosition: 'middle',
-						legendOffset: 40,
-				}}
-				axisLeft={{
-						tickSize: 5,
-						tickPadding: 5,
-						tickRotation: 0,
-						legend: 'Client',
-						legendPosition: 'middle',
-						legendOffset: -100
-				}}
-				labelSkipWidth={12}
-				labelSkipHeight={12}
-				labelTextColor={'white'}
-				legends={[
-						{
-							dataFrom: 'keys',
-							anchor: 'bottom-right',
-							direction: 'column',
-							justify: false,
-							translateX: 120,
-							translateY: 0,
-							itemsSpacing: 2,
-							itemWidth: 100,
-							itemHeight: 20,
-							itemDirection: 'left-to-right',
-							itemOpacity: 0.85,
-							itemTextColor: 'white',
-							symbolSize: 20,
-							effects: [
-								{
-										on: 'hover',
-										style: {
-											itemOpacity: 1
-										}
-								}
-							]
-						}
-				]}
-				animate={true}
-				motionStiffness={90}
-				motionDamping={15}
-			/>
+			<div className='chart-container'>
+				<ResponsiveBar
+					data={props.expeditedCount}
+					keys={[ 'Non-Expedited', 'Expedited' ]}
+					indexBy="client"
+					margin={{ top: 5, right: 150, bottom: 50, left: 100 }}
+					padding={0.3}
+					layout="vertical"
+					colors={['#8b2abb', '#ef7a00']}
+					theme = {theme}
+					// borderWidth={3}
+					// borderColor={'white'}
+					borderRadius={5}
+					index='color'
+					axisTop={null}
+					axisRight={null}
+					axisBottom={{
+							tickSize: 5,
+							tickPadding: 5,
+							tickRotation: 0,
+							legend: 'Client',
+							legendPosition: 'middle',
+							legendOffset: 40,
+					}}
+					axisLeft={{
+							tickSize: 5,
+							tickPadding: 5,
+							tickRotation: 0,
+							legend: 'Total',
+							legendPosition: 'middle',
+							legendOffset: -40,
+					}}
+					labelSkipWidth={12}
+					labelSkipHeight={12}
+					labelTextColor={'white'}
+					legends={[
+							{
+								dataFrom: 'keys',
+								anchor: 'bottom-right',
+								direction: 'column',
+								justify: false,
+								translateX: 120,
+								translateY: 0,
+								itemsSpacing: 2,
+								itemWidth: 100,
+								itemHeight: 20,
+								itemDirection: 'left-to-right',
+								itemOpacity: 0.85,
+								itemTextColor: 'white',
+								symbolSize: 20,
+								effects: [
+									{
+											on: 'hover',
+											style: {
+												itemOpacity: 1
+											}
+									}
+								]
+							}
+					]}
+					animate={true}
+					motionStiffness={90}
+					motionDamping={15}
+				/>
+			</div>
 		</div>
 	)
 }
