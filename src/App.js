306a76e181669e2e5b5a0d5eedbd3d150760ba0e
Kyle Payne <kspayne93@gmail.com>
2020-03-26 23:31:27 -0600

    refactored

--- src/App.js
+++ src/App.js
@@ -10,62 +10,61 @@ function App() {
    const [expeditedCount, setExpeditedCount] = useState([]);
    const [highPriorities, setHighPriorities] = useState([]);
 
-   useEffect(() => {
-      const getOrderData = async () => {
-         try {
-            let res = await axios.get('https://stuckorders.mobilsense.com/api/stuck_orders');
-            setOrderData(res.data);
-            console.log(res.data.stuck_orders)
-         } catch (err) {
-            console.log(err);
-         }
-      };
-      getOrderData();
-   }, []);
+   const getOrderData = async () => {
+      try {
+         let res = await axios.get('https://stuckorders.mobilsense.com/api/stuck_orders');
+         setOrderData(res.data);
+         console.log(res.data.stuck_orders)
+      } catch (err) {
+         console.log(err);
+      }
+   };
 
-   useEffect(() => {
-      const getExpeditedCount = () => {
-         let clients = {};
-         if (orderData) {
-            orderData.stuck_orders.forEach(order => {
-               clients[order.client] = clients[order.client] || {client: order.client, "Expedited": 0, "Non-Expedited": 0};
-               if (order.expedited) {
-                  clients[order.client]['Expedited']++;
-               } else {
-                  clients[order.client]['Non-Expedited']++;
-               }
-            })
-         }
-         const clientsArr = [];
-         for (let client in clients) {
-            clientsArr.push(clients[client]);
-         }
-         setExpeditedCount(clientsArr);
-      };
+   const getExpeditedCount = (orderData) => {
+      let clients = {};
+      if (orderData) {
+         orderData.stuck_orders.forEach(order => {
+            clients[order.client] = clients[order.client] || {client: order.client, "Expedited": 0, "Non-Expedited": 0};
+            if (order.expedited) {
+               clients[order.client]['Expedited']++;
+            } else {
+               clients[order.client]['Non-Expedited']++;
+            }
+         })
+      }
+      const clientsArr = [];
+      for (let client in clients) {
+         clientsArr.push(clients[client]);
+      }
+      return clientsArr;
+   };
 
-      getExpeditedCount();
-   }, [orderData]);
-
-   useEffect(() => {
-      const getHighPriorities = () => {
-         if (!orderData) return;
-
-         // older than 2 days
-         let highPriorities = orderData.stuck_orders.filter(order => {
+   const getHighPriorities = (orderData) => {
+      let priorities = [];
+      if (orderData) {
+         priorities = orderData.stuck_orders.filter(order => {
             let orderDate = new Date(order.order_timestamp);
             let today = new Date();
-            if (today.getDate() - orderDate.getDate() > 2) {
+            if (today.getDate() - orderDate.getDate() > 2) { // older than 2 days
                return true;
             } else {
                return false;
             }
          });
+      }
+      return priorities;
+   };
 
-         setHighPriorities(highPriorities);
-      };
+   useEffect(() => {
+      getOrderData();
+   }, []);
 
-      getHighPriorities();
-   }, [orderData])
+   useEffect(() => {
+      let clientsArr = getExpeditedCount(orderData);
+      let prioritiesArr = getHighPriorities(orderData);
+      setExpeditedCount(clientsArr);
+      setHighPriorities(prioritiesArr);
+   }, [orderData]);
 
    return (
       <div className='app'>
