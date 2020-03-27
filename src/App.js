cdb247b134c412f694b4be85050485fc2f00559d
Kyle Payne <kspayne93@gmail.com>
2020-03-27 13:23:42 -0600

    day count panel data & styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 import ClientCountPanel from './components/panels/ClientCountPanel/ClientCountPanel';
 import HighPriorityPanel from './components/panels/HighPriorityPanel/HighPriorityPanel';
+import DayCountPanel from './components/panels/DayCountPanel/DayCountPanel';
 
 import axios from 'axios';
 
@@ -9,6 +10,7 @@ function App() {
    const [orderData, setOrderData] = useState();
    const [expeditedCount, setExpeditedCount] = useState([]);
    const [highPriorities, setHighPriorities] = useState([]);
+   const [dayCount, setDayCount] = useState([]);
 
    const getOrderData = async () => {
       try {
@@ -24,7 +26,7 @@ function App() {
       let clients = {};
       if (orderData) {
          orderData.stuck_orders.forEach(order => {
-            clients[order.client] = clients[order.client] || {client: order.client, "Expedited": 0, "Non-Expedited": 0};
+            clients[order.client] = clients[order.client] || {client: order.client_db_name, "Expedited": 0, "Non-Expedited": 0};
             if (order.expedited) {
                clients[order.client]['Expedited']++;
             } else {
@@ -43,8 +45,8 @@ function App() {
       let priorities = [];
       if (orderData) {
          priorities = orderData.stuck_orders.filter(order => {
-            let orderDate = new Date(order.order_timestamp);
             let today = new Date();
+            let orderDate = new Date(order.order_timestamp);
             if (today.getDate() - orderDate.getDate() > 2) { // older than 2 days
                return true;
             } else {
@@ -55,22 +57,42 @@ function App() {
       return priorities;
    };
 
+   const getDayCount = (orderData) => {
+      let days = {};
+      if (orderData) {
+         orderData.stuck_orders.forEach(order => {
+            let today = new Date();
+            let orderDate = new Date(order.order_timestamp);
+            let difference = (today.getDate() - orderDate.getDate()).toString();
+            days[difference] = days[difference] || {day: difference, count: 0};
+            days[difference].count++;
+         })
+      }
+      const daysArr = [];
+      for (let day in days) {
+         daysArr.push(days[day]);
+      }
+      return daysArr;
+   };
+
    useEffect(() => {
       getOrderData();
    }, []);
 
    useEffect(() => {
-      let clientsArr = getExpeditedCount(orderData);
-      let prioritiesArr = getHighPriorities(orderData);
+      const clientsArr = getExpeditedCount(orderData);
+      const prioritiesArr = getHighPriorities(orderData);
+      const daysArr = getDayCount(orderData);
       setExpeditedCount(clientsArr);
       setHighPriorities(prioritiesArr);
+      setDayCount(daysArr);
    }, [orderData]);
 
    return (
       <div className='app'>
          <ClientCountPanel expeditedCount={expeditedCount} />
          <HighPriorityPanel highPriorities={highPriorities} />
-         <div className='dashboard-panel'></div>
+         <DayCountPanel dayCount={dayCount} />
          <div className='dashboard-panel'></div>
       </div>
    );
